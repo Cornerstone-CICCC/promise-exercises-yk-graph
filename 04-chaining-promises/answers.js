4fc86d1b087478bbb248aa0a7b9c4c5108b4f9f4
yk-graph <yokokura.tatsuya@gmail.com>
2026-05-27 19:10:24 -0700

    fix: 04-chaining-promises

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,43 +1,57 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
-    promise
-      .then(/* IMPLEMENT ME! */);
-  });
+    promise.then(
+      (val) => {
+        asyncTransformer(val).then(resolve, reject)
+      },
+      (err) => {
+        reject(err)
+      },
+    )
+  })
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then((val) => slowAsyncProcess(val))
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
-  };
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function getUserByIdWithOrganization(userId) {
+    return getUserById(userId).then((user) => {
+      if (!user) {
+        return undefined
+      }
+      return getOrganizationById(user.organizationId).then((organization) => ({
+        ...user,
+        organization,
+      }))
+    })
+  }
 }
 
 module.exports = {
   flatMapPromise,
   chainTwoAsyncProcesses,
   makeGetUserByIdWithOrganization,
-};
+}
